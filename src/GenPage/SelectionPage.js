699a331f074020b7153735c10d2e6f505e180314
Nick Pysklywec <npysklyw@uwo.ca>
2022-02-01 16:20:13 -0500

    Changed

--- src/GenPage/SelectionPage.js
+++ src/GenPage/SelectionPage.js
@@ -7,7 +7,15 @@ function SelectionPage() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>hello</div>
+        <div>
+          <h1>Select Your Options</h1>
+          <p1>Choose Your Algorithm</p1>
+          <p1>Select a Level</p1>
+          <p1>List Size</p1>
+          <button>Start</button>
+
+
+        </div>
       </header>
     </div>
   );
