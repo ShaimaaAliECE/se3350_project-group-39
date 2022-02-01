b927db19a25f4245d989a8358bfb849396a8c035
aadhieaswar <aadhieaswar@gmail.com>
2022-01-31 21:39:58 -0500

    set up virtual environment

--- api/app.py
+++ api/app.py
@@ -3,6 +3,11 @@ import time
 
 app = Flask(__name__)
 
+# default route
+@app.route('/')
+def index():
+    return "<h1 style=\"text-align:center\">Welcome to KnowYourAlgo's Backend</h1>"
+
 @app.route('/time')
-def getTime(): 
+def getTime():
     return {'time': time.time()}
