d91f9d12af64a61532132db9c79ad5c283600529
Nick Pysklywec <npysklyw@uwo.ca>
2022-02-01 17:09:22 -0500

    Making it More Presentable

--- src/GenPage/SelectionPage.js
+++ src/GenPage/SelectionPage.js
@@ -1,17 +1,81 @@
 import './selectionPage.css';
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Menu, Dropdown,Slider, Switch} from 'antd';
+import "antd/dist/antd.css";
+
+import { PageHeader } from 'antd';
+
+const menu = (
+  <Menu className="Menu"> 
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">Merge Sort</a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">Quick Sort</a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">Bubble Sort</a>
+    </Menu.Item>
+  </Menu>
+);
+
+const level = (
+  <Menu className="Menu"> 
+    <Menu.Item>1</Menu.Item>
+    <Menu.Item>2</Menu.Item>
+    <Menu.Item>3</Menu.Item>
+  </Menu>
+);
+
+const ListSize = (
+  <Menu className="Menu"> 
+    <Menu.Item>1</Menu.Item>
+    <Menu.Item>2</Menu.Item>
+    <Menu.Item>3</Menu.Item>
+  </Menu>
+);
+
+function onChange(value) {
+  console.log('onChange: ', value);
+}
+
+function onAfterChange(value) {
+  console.log('onAfterChange: ', value);
+}
 
 function SelectionPage() {
-
+  const { disabled } = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <h1>Select Your Options</h1>
-          <p1>Choose Your Algorithm</p1>
-          <p1>Select a Level</p1>
-          <p1>List Size</p1>
+    <div >
+      <header className="App">
+        <div className="App-header">
+                  <PageHeader
+                    className="site-page-header"
+                    title="Select Your Algo"
+                    />
+              <div style={{"padding":"10px","theme": "#1DA57A"}}>
+              <Dropdown overlay={menu}>
+                  <a className="ant-dropdown-link" href="#">
+                    Choose an Algo Here
+                  </a>
+                </Dropdown>
+              </div>
+
+              <div style={{"padding":"10px","theme": "#1DA57A"}}>
+             
+              </div>
+              <div style={{
+      display: 'block', width: 700, padding: 30
+    }}>
+      <h2>Select a Level</h2>
+      <Slider defaultValue={0} disabled={false} max={10} 
+      />
+      Slider Value: {4}
+    </div>
+   
+                  
+ 
           <button>Start</button>
 
 
