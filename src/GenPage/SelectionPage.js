8a0b8125879ef4cff088c7bc93b4655eda5a015a
Nick Pysklywec <npysklyw@uwo.ca>
2022-02-08 17:05:13 -0500

    Adding in Images per algo in selection

--- src/GenPage/SelectionPage.js
+++ src/GenPage/SelectionPage.js
@@ -1,7 +1,7 @@
-import "./selectionPage.css";
+import "./SelectionPage.css";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Slider, Switch, PageHeader, Select, Image } from "antd";
+import { Slider,PageHeader, Select, Image } from "antd";
 import "antd/dist/antd.css";
 import useToken from '../useToken';
 import Header from "../Header/Header";
@@ -11,9 +11,15 @@ const { Option } = Select;
 function SelectionPage() {
   const [level, setLevel] = useState(0);
   const [listSize, setListSize] = useState(0);
-  const [algo, setAlgo] = useState("");
+  const [algo, setAlgo] = useState("b");
   const navigate = useNavigate();
   const {removeToken} = useToken();
+
+  const sortImage = {
+    b: "https://cdn.programiz.com/cdn/farfuture/kn1zM7ZGIj60jcTe3mv8gAtbrvFHqxgqfQ7F9MdjPuA/mtime:1582112622/sites/tutorial2program/files/Bubble-sort-0.png",
+    q: "https://cdn.programiz.com/cdn/farfuture/QA-TsXFkcz3cNyJikcbIWxepFVDu8ntl220KzlG8zdw/mtime:1617189492/sites/tutorial2program/files/quick-sort-partition-third-step.png",
+    m: "https://cdn.programiz.com/cdn/farfuture/PRTu8e23Uz212XPrrzN_uqXkVZVY_E0Ta8GZp61-zvw/mtime:1586425911/sites/tutorial2program/files/merge-sort-example_0.png"
+  }
 
   return (
     <div>
@@ -21,20 +27,21 @@ function SelectionPage() {
       <div className="App">
           <div className="App-header">
                     
+          <Header />      
           <PageHeader className="site-page-header" title="Select Your Algo" />
 
 
-          <div className="loginFrame">
-          <p className="sign" align="center">Select a Algorithm</p>
-            
+          <div className="Frame">
 
+            <p className="sign" align="center">Select a Algorithm</p>
+            
             <div align="center" style={{padding:"10px"}}>
               
             <Select
               defaultValue="b"
               style={{ width: '75%' }}
               onChange={(value) => {
-                console.log(value);
+                console.log(value)
                 setAlgo(value);
               }}
             >
@@ -46,8 +53,9 @@ function SelectionPage() {
 
             <div align="center">
             <Image align="center" style={{padding:"10px"}}
-              width={'25%'}
-              src="https://cdn.programiz.com/cdn/farfuture/PRTu8e23Uz212XPrrzN_uqXkVZVY_E0Ta8GZp61-zvw/mtime:1586425911/sites/tutorial2program/files/merge-sort-example_0.png"
+              width={'50%'}
+              src={sortImage[algo]}
+              fallback="https://cdn.programiz.com/cdn/farfuture/QA-TsXFkcz3cNyJikcbIWxepFVDu8ntl220KzlG8zdw/mtime:1617189492/sites/tutorial2program/files/quick-sort-partition-third-step.png"
             />
             </div>
 
