f4e79e93ef3141703ce3697f897a28906b2678a2
aadhieaswar <aadhieaswar@gmail.com>
2022-02-10 19:43:49 -0500

    adding sqlite db to the project

--- api/.flaskenv
+++ api/.flaskenv
@@ -1,3 +1,2 @@
 FLASK_APP=app.py
 FLASK_ENV=development
-DATABASE_URL=mysql:///?User=root&Password=password&Database=kya&Server=localhost&Port=3306
--- api/db.py
+++ api/db.py
@@ -1,36 +1,16 @@
-# function to get the user from the email given
-def get_user(db, email):
-    # construct the query
-    query = "SELECT * FROM Users WHERE email=:email"
+import sqlite3
 
-    # execute the query
-    result = db.execute(query, { "email": email }).fetchone()
+# method to create a function
+def create_connection():
+    # connect to the db file
+    conn = sqlite3.connect('./sqlite.db')
 
-    return result
+    # return the connection
+    return conn
 
-def add_user(db, email, password):
-    # construct the query
-    query = "INSERT INTO Users (email, password) VALUES (:email, :password)"
+# method to get a user using email
+def get_user(email):
+    conn = create_connection()
+    cursor = conn.cursor()
 
-    # execute the query
-    db.execute(query, { "email": email, "password": password })
-    db.commit()
-
-# function to get all the statistics associated with a user email
-def get_stats(db, email):
-    # construct the query
-    query = "SELECT * FROM Statistics WHERE email=:email"
-
-    # execute the query
-    results = db.execute(query, { "email": email }).fetchall()
-
-    return results
-
-# function to add a statistic
-def add_stats(db, email, level, algorithm, time):
-    # construct the query
-    query = "INSERT INTO Statistics (email, level, algorithm, time) VALUES (:email, :level, :algorithm, :time)"
-
-    # execute the query
-    db.execute(query, { "email": email, "level": level, "algorithm": algorithm, "time": time })
-    db.commit()
+    query = "SELECT * FROM users"
