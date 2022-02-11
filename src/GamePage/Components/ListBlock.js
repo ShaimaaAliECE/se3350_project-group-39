44242b13767657ba8f7fd8de19278c68e131c28b
anujkulkarni123 <anianuj123@gmail.com>
2022-02-10 20:14:11 -0500

    auto formatter

--- src/GamePage/Components/ListBlock.js
+++ src/GamePage/Components/ListBlock.js
@@ -1,80 +1,88 @@
-import React, { useState, useEffect } from 'react'
-import './listBlock.css'
+import React, { useState, useEffect } from "react";
+import "./listBlock.css";
 
-function ListBlocks({ blocks, compare, sorted, swap }){
+function ListBlocks({ blocks, compare, sorted, swap }) {
+  console.log(blocks);
+  const [width, setWidth] = useState(
+    Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 5)
+  );
+  const color = blocks.length <= 50 && width > 14 ? "black" : "transparent";
 
-    console.log(blocks)
-    const [width, setWidth] = useState(Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 5))
-    const color = blocks.length <= 50 && width > 14 ? 'black' : 'transparent'
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 8));
+    };
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWidth(Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 8))
-        }
+    window.addEventListener("resize", handleResize);
 
-        window.addEventListener('resize', handleResize)
+    setWidth(Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 8));
+  }, [blocks.length]);
 
-        setWidth(Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 8))
-    }, [blocks.length])
+  const renderBlocks = (block, i) => {
+    if (blocks !== undefined) {
+      const height = (block * 500) / blocks.length;
+      let bg = "turquoise";
 
+      // i th element is being compared with some other element
+      if (compare && (i === compare[0] || i === compare[1])) {
+        bg = "#ffff50";
+      }
 
-    const renderBlocks=(block, i)=> {
-        if (blocks !== undefined) {
-            const height = block * 500 / blocks.length;
-                let bg = 'turquoise'
+      if (swap && (i === swap[0] || i === swap[1])) {
+        bg = "red";
+      }
+      // i th element is in its correct position
+      if (sorted && sorted.includes(i)) {
+        bg = "#4bc52e";
+      }
 
-                // i th element is being compared with some other element
-                if(compare && (i === compare[0] || i === compare[1])){
-                    bg = '#ffff50'
-                }
-
-                if(swap && (i === swap[0] || i === swap[1])){
-                    bg='red'
-                }
-                // i th element is in its correct position
-                if(sorted && sorted.includes(i)){
-                    bg = '#4bc52e'
-                }
-
-                const style = {
-                    'backgroundColor': bg,
-                    'color': color, 
-                    'height': height, 
-                    'width': width
-                }
-                return (<div key={i} className='block' style={style}>{block}</div>)
-        }
-    }
-
-    return (
-        <div className='listBlocks'>
-        {blocks.map((block, i) => {
-        const height = block * 500 / blocks.length;
-            let bg = 'turquoise'
-
-            // i th element is being compared with some other element
-            if(compare && (i === compare[0] || i === compare[1])){
-                bg = '#ffff50'
-            }
-
-            if(swap && (i === swap[0] || i === swap[1])){
-                bg='red'
-            }
-            // i th element is in its correct position
-            if(sorted && sorted.includes(i)){
-                bg = '#4bc52e'
-            }
-
-            const style = {
-                'backgroundColor': bg,
-                'color': color, 
-                'height': height, 
-                'width': width
-            }
-            return (<div key={i} className='block' style={style}>{block}</div>)
-    })}
+      const style = {
+        backgroundColor: bg,
+        color: color,
+        height: height,
+        width: width,
+      };
+      return (
+        <div key={i} className="block" style={style}>
+          {block}
         </div>
-    );
+      );
+    }
+  };
+
+  return (
+    <div className="listBlocks">
+      {blocks.map((block, i) => {
+        const height = (block * 500) / blocks.length;
+        let bg = "turquoise";
+
+        // i th element is being compared with some other element
+        if (compare && (i === compare[0] || i === compare[1])) {
+          bg = "#ffff50";
+        }
+
+        if (swap && (i === swap[0] || i === swap[1])) {
+          bg = "red";
+        }
+        // i th element is in its correct position
+        if (sorted && sorted.includes(i)) {
+          bg = "#4bc52e";
+        }
+
+        const style = {
+          backgroundColor: bg,
+          color: color,
+          height: height,
+          width: width,
+        };
+        return (
+          <div key={i} className="block" style={style}>
+            {block}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default ListBlocks
+export default ListBlocks;
