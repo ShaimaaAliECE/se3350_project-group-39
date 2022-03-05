43841e1af8b90e6ca107a54557ddd4eba4849b0b
Andrew Schaus <andrewschaus3@gmail.com>
2022-03-05 17:28:47 -0500

    comments

--- src/GamePage/Components/level2.js
+++ src/GamePage/Components/level2.js
@@ -7,7 +7,7 @@ function Level2({ blocks, sorted, swap, needsSorting, steps }) {
     Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 5)
   );
   const [list, setList] = useState(blocks);
-  const [current, setCurrent] = useState([]);
+  const [current, setCurrent] = useState([]); //Currently highlighted blue blocks
   const color = blocks.length <= 50 && width > 14 ? "black" : "transparent";
 
   useEffect(() => {
@@ -33,6 +33,7 @@ function Level2({ blocks, sorted, swap, needsSorting, steps }) {
     setList(items);
   };
 
+  // Switches what is being stored in the current array
   const handleSteps = () =>{
     console.log(steps);
       switch(steps){
