b54e4ca13c04b39409c47f2bd5531cc4d42f3130
aadhieaswar <aadhieaswar@gmail.com>
2022-03-05 18:57:39 -0500

    minor fixes

--- src/GamePage/Components/level2.js
+++ src/GamePage/Components/level2.js
@@ -59,6 +59,8 @@ function Level2({ blocks, sorted, swap, needsSorting, steps }) {
         case 6:
           setCurrent([0,1,2,3,4,5,6,7,8,9])
           break;
+        default:
+          break;
       }
 
 
