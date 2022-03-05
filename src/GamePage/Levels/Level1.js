7be0b1f387ad3d640908f3bb966c0bba29ab9ca1
aadhieaswar <aadhieaswar@gmail.com>
2022-03-05 13:51:45 -0500

    added new file for level 1

---
+++ src/GamePage/Levels/Level1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Level1.css';
+
+export default function Level1() {
+    return (
+        <>
+            <div className='prev-next-container'>
+                <button>prev</button>
+                <button>next</button>
+            </div>
+        </>
+    );
+}
