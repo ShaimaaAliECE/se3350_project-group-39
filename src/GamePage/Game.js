0fdbb60849b31cba9699e9bf33f3b114e43805f7
Andrew Schaus <andrewschaus3@gmail.com>
2022-03-05 13:48:48 -0500

    drip

--- src/GamePage/Game.js
+++ src/GamePage/Game.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import './Game.css';
-import mergeSort from '../Algos/MergeSort'
-import ListBlocks from './Components/ListBlock';
-import axios from 'axios';
-
-=======
 import React, { useState, useEffect } from "react";
 import "./Game.css";
 import mergeSort from "../Algos/MergeSort";
@@ -14,7 +6,6 @@ import quickSort from "../Algos/QuickSort"
 import ListBlocks from "./Components/ListBlock";
 import axios from "axios";
 import { resetServerContext } from "react-beautiful-dnd";
->>>>>>> main
 
 export default function Game({ algorythm, difficulty, size, clicked }) {
     //states
@@ -22,21 +13,14 @@ export default function Game({ algorythm, difficulty, size, clicked }) {
     const [level, setLevel] = useState(difficulty);
     const [blocks, setBlocks] = useState([]);
     const [algo, setAlgo] = useState(algorythm);
-<<<<<<< HEAD
-    const [isSorting, setIsSorting] = useState();
-=======
     const [isSorting, setIsSorting] = useState(true);
->>>>>>> main
     const [speed, setSpeed] = useState(200);
     const [compare, setCompare] = useState([]);
     const [completed, setCompleted] = useState(true);
     const [sortedIndex, setSortedIndex] = useState([]);
     const [swap, setSwap] = useState([]);
 
-<<<<<<< HEAD
-=======
     // Gets random numbers from the back end and fills the blocks array with them
->>>>>>> main
     function getRandomNumbers() {
         axios({
             method: "GET",
@@ -61,36 +45,6 @@ export default function Game({ algorythm, difficulty, size, clicked }) {
         console.log("isSorting = " + isSorting); 
     }
 
-<<<<<<< HEAD
-    const handleAlgo = (event) => {
-        setAlgo(event.target.value);
-    }
-
-    useEffect(() => {
-        getRandomNumbers();
-
-    }, [clicked]);
-
-
-    function handleSort() {
-        const sortOrder = (order) => {
-            (function loop(i) {
-                setTimeout(function () {
-                    const [j, k, arr, index] = order[i]
-                    setCompare([j, k])
-                    setSwap([])
-
-                    if (index !== null) {
-                        setSortedIndex((prevState) => (
-                            [...prevState, index]
-                        ))
-                    }
-
-                    if (arr) {
-                        setBlocks(arr)
-                        if (j !== null || k != null)
-                            setSwap([j, k])
-=======
     // Called every time the start button is clicked and when the sliders are moved
     useEffect(() => {
         getRandomNumbers();
@@ -119,7 +73,6 @@ export default function Game({ algorythm, difficulty, size, clicked }) {
                     if (arr) {
                         setBlocks(arr);
                         if (j !== null || k != null) setSwap([j, k]);
->>>>>>> main
                     }
 
                     if (++i < order.length) {
@@ -145,19 +98,6 @@ export default function Game({ algorythm, difficulty, size, clicked }) {
         if (algo == "mergeSort") {
             sortOrder(mergeSort(blocks));
 
-<<<<<<< HEAD
-            setIsSorting(true);
-
-            // algo === "mergeSort"
-            //     ? sortOrder(mergeSort(blocks))
-            //     : (() => {
-            //           setIsSorting(false);
-            //           setCompleted(true);
-            //       })();
-        };
-        if (clicked)
-            sortOrder(mergeSort(blocks));
-=======
         } else if (algo == "quickSort") {
             sortOrder(quickSort(blocks));
 
@@ -167,7 +107,6 @@ export default function Game({ algorythm, difficulty, size, clicked }) {
         }
 
         
->>>>>>> main
     }
 
     // Displays all the blocks in the array
@@ -177,18 +116,10 @@ export default function Game({ algorythm, difficulty, size, clicked }) {
                 length={length}
                 blocks={blocks}
                 compare={isSorting && compare}
-<<<<<<< HEAD
-                swap={isSorting && swap}
-                sorted={sortedIndex}
-            />
-
-            <button id='startBtn' onClick={() => handleSort()}> START </button>
-=======
                 swap={swap}
                 needsSorting={isSorting}
                 sorted={sortedIndex}
             />
->>>>>>> main
         </div>
     );
 }
