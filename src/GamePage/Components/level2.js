72f2a0120448e6984f4066057768315046129412
aadhieaswar <aadhieaswar@gmail.com>
2022-03-08 18:42:56 -0500

    major fix for level 2

--- src/GamePage/Components/level2.js
+++ src/GamePage/Components/level2.js
@@ -3,29 +3,35 @@ import { DragDropContext, Droppable, Draggable, resetServerContext } from "react
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import "./listBlock.css";
 
-function Level2({ blocks, sorted, swap, needsSorting, steps, countUp }) {
+function Level2({ blocks, sorted, swap, needsSorting, steps, countUp, countDown }) {
     const [width, setWidth] = useState(
     Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 5)
   );
   const [list, setList] = useState(blocks);
   const [current, setCurrent] = useState([]); //Currently highlighted blue blocks
+  const [currestStepValid, setCurrentStepValid] = useState(false);
   const color = blocks.length <= 50 && width > 14 ? "black" : "transparent";
   let dropOrNotToDrop = true;
 
   useEffect(() => {
+    setCurrentStepValid(false);
     handleSteps();
+    checkCurrentStep(list);
   }, [steps]);
 
   useEffect(() => {
     setWidth(
         Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 8)
       );
-        setList(blocks);
-  }, [blocks])
-
+      setList(blocks);
+    setCurrentStepValid(false);
+    checkCurrentStep(blocks);
+  }, [blocks]);
 
   const handleOnDragEnd = (result) => {
     if (!result.destination) return;
+
+    if (!current.includes(result.destination.index)) return;
 
     const items = Array.from(list);
     console.log(items)
@@ -33,6 +39,8 @@ function Level2({ blocks, sorted, swap, needsSorting, steps, countUp }) {
     items.splice(result.destination.index, 0, reorderedItem);
 
     setList(items);
+
+    checkCurrentStep(items);
   };
 
   // Switches what is being stored in the current array
@@ -63,14 +71,36 @@ function Level2({ blocks, sorted, swap, needsSorting, steps, countUp }) {
         default:
           break;
       }
+  }
 
+  function checkCurrentStep(items) {
+    let array = items.slice(current[0], current[current.length - 1] + 1);
 
+    let sortedArray = JSON.parse(JSON.stringify(array));
+    sortedArray.sort((first, second) => first - second);
+    let isEqual = true;
+
+    console.log(array);
+    console.log(sortedArray);
+    
+    array.forEach((item, index) => {
+      if (!(sortedArray[index] === item)) {
+        isEqual = false;
+        return;
+      }
+    });
+
+    if (isEqual) {
+      setCurrentStepValid(true);
+    } else {
+      setCurrentStepValid(false);
+    }
   }
 
   return (
     <div>
       <div className='prev-next-container'>
-          <button><FaAngleLeft /></button>
+          <button onClick={countDown}><FaAngleLeft /></button>
           <button onClick={countUp}><FaAngleRight /></button>
       </div>
       <DragDropContext onDragEnd={handleOnDragEnd}>
@@ -92,10 +122,10 @@ function Level2({ blocks, sorted, swap, needsSorting, steps, countUp }) {
                 }
 
                   if(current.includes(index)) {
-                    bg="yellow";
+                    bg = ( currestStepValid ? "#4bc52e" : "yellow" );
                     dropOrNotToDrop = false;
                   } else {
-                    bg="black";
+                    bg = "black";
                     dropOrNotToDrop = true;
                   }
 
@@ -112,15 +142,15 @@ function Level2({ blocks, sorted, swap, needsSorting, steps, countUp }) {
                   {
                     if(checkSort(list)) {
                       bg = "#4bc52e"
-                      dropOrNotToDrop = false;
-                    }
-                    else
+                      dropOrNotToDrop = true;
+                    } else {
                       bg = "red"
+                    }
                     console.log(steps)
                   }
                 
                 const style = {
-                  backgroundColor: bg,
+                  // backgroundColor: bg,
                   color: color,
                   height: height,
                   width: width,
@@ -141,7 +171,11 @@ function Level2({ blocks, sorted, swap, needsSorting, steps, countUp }) {
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
                       >
-                        <div style={style}>{block}</div>
+                        <div
+                          style={{ backgroundColor: current.includes(index) ? currestStepValid ? "#4bc52e" : "yellow" : bg || "black", ...style }}
+                        >
+                          {block}
+                        </div>
                       </li>
                     )}}
                   </Draggable>
