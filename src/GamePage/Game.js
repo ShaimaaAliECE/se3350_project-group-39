5350a38cf294e08f1a4eab844e693d1b6b0fa9f6
richardphan01 <richard.phan01@gmail.com>
2022-03-22 13:00:24 -0400

    finished level 5

--- src/GamePage/Game.js
+++ src/GamePage/Game.js
@@ -6,6 +6,7 @@ import Level2 from "./Components/Level2";
 import Level3 from "./Components/Level3";
 import Level4 from "./Components/Level4";
 import Timer from "../GenPage/Timer";
+import Level5 from "./Components/Level5";
 
 export default function Game({ algorithm, difficulty, size, clicked }) {
     // states
@@ -22,7 +23,7 @@ export default function Game({ algorithm, difficulty, size, clicked }) {
                 min: 1,
                 max: 20
             },
-            
+
         }).then(({ data }) => {
             setBlocks(data);
         });
@@ -39,43 +40,31 @@ export default function Game({ algorithm, difficulty, size, clicked }) {
     }, [clicked, size]);
 
     // Increments steps according to how many steps there are
-    function counter(){
+    function counter() {
         console.log(steps)
-        setSteps(steps+1);
-        if(steps >= 7 && difficulty < 4){
+        setSteps(steps + 1);
+        if (steps >= 7 && difficulty < 4) {
             setSteps(0);
         }
-        if(steps >= 14 && difficulty === 4){
+        if (steps >= 14 && difficulty === 4) {
             setSteps(0);
         }
     }
 
     function countDown() {
-        setSteps(steps-1);
-        if(steps <= 0){
+        setSteps(steps - 1);
+        if (steps <= 0) {
             setSteps(0);
         }
- 
+
     }
 
     return (
         <div className="game" id="game-body">
-            { difficulty === 1 ? 
+            {difficulty === 1 ?
                 <Level1 />
-            : difficulty === 2 ? 
-                <Level2
-                    blocks={blocks}
-                    current={true}
-                    steps={steps}
-                    countUp={counter}
-                    countDown={countDown}
-                    algorithm={algorithm}
-                    level={difficulty}
-                />
-            : difficulty === 3 ? 
-                <>
-                <div className="stepCounter"> Step {steps + 1}</div>
-                    <Level3
+                : difficulty === 2 ?
+                    <Level2
                         blocks={blocks}
                         current={true}
                         steps={steps}
@@ -84,21 +73,47 @@ export default function Game({ algorithm, difficulty, size, clicked }) {
                         algorithm={algorithm}
                         level={difficulty}
                     />
-                </>
-            : difficulty === 4 ? 
-                <>
-                <div className="stepCounter"> Step {steps + 1}</div>
-                    <Level4
-                        blocks={blocks}
-                        current={true}
-                        steps={steps}
-                        countUp={counter}
-                        countDown={countDown}
-                        algorithm={algorithm}
-                        level={difficulty}
-                    />
-                </>
-            : <></>}
+                    : difficulty === 3 ?
+                        <>
+                            <div className="stepCounter"> Step {steps + 1}</div>
+                            <Level3
+                                blocks={blocks}
+                                current={true}
+                                steps={steps}
+                                countUp={counter}
+                                countDown={countDown}
+                                algorithm={algorithm}
+                                level={difficulty}
+                            />
+                        </>
+
+                        : difficulty === 4 ?
+                            <>
+                                <div className="stepCounter"> Step {steps + 1}</div>
+                                <Level4
+                                    blocks={blocks}
+                                    current={true}
+                                    steps={steps}
+                                    countUp={counter}
+                                    countDown={countDown}
+                                    algorithm={algorithm}
+                                    level={difficulty}
+                                />
+                            </>
+                            : difficulty === 5 ?
+                                <>
+                                    <div className="stepCounter"> Step {steps + 1}</div>
+                                    <Level5
+                                        blocks={blocks}
+                                        current={true}
+                                        steps={steps}
+                                        countUp={counter}
+                                        countDown={countDown}
+                                        algorithm={algorithm}
+                                        level={difficulty}
+                                    />
+                                </>
+                                : <></>}
         </div>
     );
 }
