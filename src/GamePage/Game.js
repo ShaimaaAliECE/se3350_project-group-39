feef6ac2c1469eb70d4be694ab4d827c3ab2d9b2
richardphan01 <richard.phan01@gmail.com>
2022-03-24 20:40:32 -0400

    fixed merge conflicts

--- src/GamePage/Game.js
+++ src/GamePage/Game.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import React, { useState, useEffect, createRef } from "react";
-import "./Game.css";
-=======
 import React, { useState, useEffect } from "react";
->>>>>>> 3dcd2f5b6c4f75a00c6a34ffa06ff24838fd6793
 import axios from "axios";
 import Level1 from "./Levels/Level1"
 import Level2 from "./Components/Level2";
@@ -11,19 +6,13 @@ import Level3 from "./Components/Level3";
 import Level4 from "./Components/Level4";
 import Timer from "../GenPage/Timer";
 
-<<<<<<< HEAD
-export default function Game({ algorithm, difficulty, size, clicked }) {
-=======
 export default function Game({ algorithm, difficulty, size, clicked, refreshLevel, max }) {
->>>>>>> 3dcd2f5b6c4f75a00c6a34ffa06ff24838fd6793
     // states
     const [blocks, setBlocks] = useState([]);
     const [steps, setSteps] = useState(0);
 
     // Gets random numbers from the back end and fills the blocks array with them
     function getRandomNumbers() {
-<<<<<<< HEAD
-=======
         
         // Determines what the max is depedning on the level
         if(difficulty <=3)
@@ -34,7 +23,6 @@ export default function Game({ algorithm, difficulty, size, clicked, refreshLeve
             max = 99;
         
             
->>>>>>> 3dcd2f5b6c4f75a00c6a34ffa06ff24838fd6793
         axios({
             method: "GET",
             url: "/random",
@@ -62,17 +50,7 @@ export default function Game({ algorithm, difficulty, size, clicked, refreshLeve
     // Increments steps according to how many steps there are
     function counter(){
         console.log(steps)
-<<<<<<< HEAD
-        setSteps(steps+1);
-        if(steps >= 7 && difficulty < 4){
-            setSteps(0);
-        }
-        if(steps >= 14 && difficulty === 4){
-            setSteps(0);
-        }
-=======
         setSteps(steps + 1);
->>>>>>> 3dcd2f5b6c4f75a00c6a34ffa06ff24838fd6793
     }
 
     function countDown() {
